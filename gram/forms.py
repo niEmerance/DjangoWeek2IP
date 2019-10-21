3c8f1832426bd856764e0f8386968dacc338bc42
niEmerance <niyemera6@gmail.com>
2019-10-21 15:53:11 +0300

    add comment

--- gram/forms.py
+++ gram/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post,Comment
 
 
 class RegisterForm(UserCreationForm):
@@ -12,4 +12,12 @@ class RegisterForm(UserCreationForm):
         model=User
         fields=["username","email","password1","password2"]
 
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['pub_date']
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comment
+        exclude=['username','post']
--- gram/views.py
+++ gram/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
-from .forms import RegisterForm,NewPostForm
+from .forms import RegisterForm,NewPostForm,CommentForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .models import Post
+from .models import Post,Comment
 from django.core.exceptions import ObjectDoesNotExist
 
 @login_required(login_url='login/')
